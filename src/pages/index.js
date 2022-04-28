4566b05b2ae40ea4ff9af2d347ceeb20527b5d55
jcmsaints <joaocm.santos@hotmail.com>
2022-04-28 20:00:13 +0100

    add microsoft clarity

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,6 +36,9 @@ const IndexPage = ({ data }) => (
 
         gtag('config', 'G-DNYR5XDG2H');`}
       </script>
+      <script>
+        {`(function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "bo771inyeh");`}
+      </script>
       <meta
         name="google-site-verification"
         content="rSyIix3thcMTM9NDtS7DsAVbA9Tzpn48KcQWIdiBOGc"
